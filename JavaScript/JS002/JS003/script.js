2e495544b79fef3d138bbed93b636d223ced2e4d
Menno <81242370+mgmeertens@users.noreply.github.com>
2021-07-23 14:29:57 +0200

    Add files via upload

---
+++ JavaScript/JS002/JS003/script.js
@@ -0,0 +1,28 @@
+/*
+const age = 18
+const isFemale = "true"
+const driverStatus = "bob"*/
+
+const age = 13; 
+const isFemale = false;
+const driverStatus = "drunk"; 
+
+
+if (age >= 18) {
+    console.log("jij mag naar binnen");
+} else {
+    console.log("Helaas je bent nog te jong om binnen te komen");
+
+}
+
+if (isFemale) {
+    console.log("jij bent een vrouw en mag naar binnen");
+} else {
+    console.log("Helaas jij bent een man en mag niet naar binnen");
+}
+
+if (driverStatus === "bob") {
+    console.log("jij bent bob dus je mag rijden");
+} else {
+    console.log("je bent dronken en mag dus niet autorijden");
+}
