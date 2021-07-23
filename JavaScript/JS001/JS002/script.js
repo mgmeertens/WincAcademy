187a98a7903ad305baee1c0b58103f5a73f077e1
Menno <81242370+mgmeertens@users.noreply.github.com>
2021-07-23 11:46:35 +0200

    Add files via upload

---
+++ JavaScript/JS001/JS002/script.js
@@ -0,0 +1,12 @@
+//n Dit is een grote som
+
+console.log("Hallo Winc Academy studenten")
+
+console.log(1230941 * 1823794);
+console.log(637263 / 54);
+
+
+/*
+console.log(1230941 * 1823794);
+console.log(637263 / 54); */
+
