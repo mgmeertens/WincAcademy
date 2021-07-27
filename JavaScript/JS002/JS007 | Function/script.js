e1b093a7371c7f96b45c6d775bcf548f0d7b74de
Menno <81242370+mgmeertens@users.noreply.github.com>
2021-07-27 17:26:11 +0200

    Add files via upload

---
+++ JavaScript/JS002/JS007 | Function/script.js
@@ -0,0 +1,22 @@
+/*const makingPizza = function() {
+    console.log("make the dough");
+    console.log("prepare toppings");
+    console.log("put toppings on pizza dough");
+    console.log("put the pizza in the oven");
+    console.log("your pizza is ready");
+}
+
+makingPizza();
+makingPizza();
+makingPizza();
+*/
+
+const freshSushi = function() {
+    console.log("cook the rice");
+    console.log("cut the fish & cool down the rice");
+    console.log("put the fish on the top of the rice");
+    console.log("mix the soy sauce with the wasabi");
+    console.log("enjoy it with a glass sake");
+}
+
+freshSushi();
