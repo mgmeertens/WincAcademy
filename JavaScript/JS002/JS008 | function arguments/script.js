30e970e767e7734500d848190153cbfe3f637bd5
Menno <81242370+mgmeertens@users.noreply.github.com>
2021-07-28 10:41:55 +0200

    Add files via upload

---
+++ JavaScript/JS002/JS008 | function arguments/script.js
@@ -0,0 +1,15 @@
+const wallPaint = function() {
+    console.log("The wall has painted:" + Color);
+    console.log("The north wall has painted Orange");
+    console.log("The west wall has painted grey");
+}
+
+wallPaint("green");
+wallPaint("purple");
+wallPaint("no color");
+
+
+
+
+
+
