802a005dbb307e951ed35e1bf50aa14a7d437fb6
Menno <81242370+mgmeertens@users.noreply.github.com>
2021-09-01 15:17:09 +0200

    Delete JS012 | While & For loops directory

--- JS012 | While & For loops/even-odd-reporter.js
+++
@@ -1,9 +0,0 @@
-
-
-for (number = 1; number <= 20; number++) {
-    if (number % 2 == 0) {
-        console.log ("is even");
-    } esle 
-        console.log ("is oneven");
-    }
-
--- JS012 | While & For loops/multiplication-tables.js
+++
@@ -1,9 +0,0 @@
-var a = 9;
-var b;
-
-for (var i = 1; i <= 10; i++) {
-
-    b = a * i;
-
-    document.write(" "+a+ "*" +i+ "=", + b +"<br>");
-}
