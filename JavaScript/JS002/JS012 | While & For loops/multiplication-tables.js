92aedda9e5ad46ae91f6a897e6d64b58b5db5765
Menno <81242370+mgmeertens@users.noreply.github.com>
2021-09-01 15:24:20 +0200

    Update multiplication-tables.js

--- JavaScript/JS002/JS012 | While & For loops/multiplication-tables.js
+++ JavaScript/JS002/JS012 | While & For loops/multiplication-tables.js
@@ -5,5 +5,23 @@ for (var i = 1; i <= 10; i++) {
 
     b = a * i;
 
-    document.write(" "+a+ "*" +i+ "=", + b +"<br>");
+    console.log(" "+a+ "*" +i+ "=", + b +"<br>");
 }
+
+/* Oplossing
+
+// Multiplication tables
+const multiplier = 9;
+for (let i = 0; i <= 10; i++) {
+  let result = multiplier * i;
+  console.log(multiplier + " * " + i + " = " + result);
+}
+
+// Bonus
+for (let multiplier = 0; multiplier <= 10; multiplier++) {
+  for (let i = 0; i <= 10; i++) {
+    let result = multiplier * i;
+    console.log(multiplier + " * " + i + " = " + result);
+  }
+  
+  */
